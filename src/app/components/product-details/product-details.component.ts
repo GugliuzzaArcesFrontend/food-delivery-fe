adcea59fec28a62982736ffc293b295abae7d3d4
ilbeccochesono <gruppostudioarces@gmail.com>
2024-06-24 09:51:49 +0200

    Aggiunto navback a dettagli prodotto

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../interfaces/product';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
@@ -10,22 +11,27 @@ import { Product } from '../../interfaces/product';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent implements OnInit,OnDestroy {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
   id!: number
-  product?:Product
+  product?: Product
   private sub!: any
-
+  
   constructor(
     private route: ActivatedRoute,
-    private productsService:ProductsService,
-  ){}
-
+    private productsService: ProductsService,
+    private location:Location
+  ) { }
+  
   ngOnInit(): void {
-    this.sub=this.route.params.subscribe(params=>this.id=params['productId']);
-    this.productsService.getProductById(this.id).subscribe((product:Product)=>this.product=product)
+    this.sub = this.route.params.subscribe(params => this.id = params['productId']);
+    this.productsService.getProductById(this.id).subscribe((product: Product) => this.product = product)
   }
-
+  
   ngOnDestroy(): void {
     this.sub.unsubscribe()
+  }
+
+  navback() {
+    this.location.back()
   }
 }
